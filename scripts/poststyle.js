148d70ec344cadd44045ca65423aba62c12e4357
Settle <whitelily.fs@gmail.com>
2025-07-12 21:05:38 -0500

    syntax issue

--- scripts/poststyle.js
+++ scripts/poststyle.js
@@ -183,7 +183,7 @@ function bc_post_style(...args) {
 
   var is_ie = !!navigator.userAgent.match(/msie/i);
   var ieVersion=(!!navigator.userAgent.match(/msie/i))?navigator.userAgent.split(/MSIE/i)[1].split(/;/)[0]:0,
-  var jBBCode = () => {
+  jBBCode = () => {
     //quick reference this
     var bb = this;
     //addTag overloaded function
